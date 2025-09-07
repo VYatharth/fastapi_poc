22887d27bfc3de0371dfcdca2a9da001c83646b7
Yathartha <varshneya.yathartha@gmail.com>
2025-09-08 04:20:11 +0530

    cleanup old code

--- MySQL/my_app/domain/usecases/read_portfolio_use_case.py
+++
@@ -1,6 +0,0 @@
-from my_app.common.models.portfolio_dto import PortfolioDto
-from my_app.domain.repository_interfaces.portfolio_repository import PortfolioRepository
-
-def read_portfolio_use_case(repo: PortfolioRepository ) -> PortfolioDto:
-    return repo.get_portfolio()
-    
