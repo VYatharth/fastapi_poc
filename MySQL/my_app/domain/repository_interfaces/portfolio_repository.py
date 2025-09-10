e9c2bf3f125fe9144297702e1c9d5595422a95a5
Yathartha <varshneya.yathartha@gmail.com>
2025-09-10 09:22:31 +0530

    more cleanup

--- MySQL/my_app/domain/repository_interfaces/portfolio_repository.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from my_app.common.models.portfolio_dto import PortfolioDto
-
-
-class PortfolioRepository(ABC):
-    @abstractmethod
-    def get_portfolio(self) -> PortfolioDto:
-        pass
