0837051eaa59101da96f19c461ff9ca2d611dcdf
Yathartha <varshneya.yathartha@gmail.com>
2025-09-11 00:08:50 +0530

    user apis working

--- MySQL/my_app/api/dependencies.py
+++ MySQL/my_app/api/dependencies.py
@@ -1,11 +1,24 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from my_app.data.repositories.user_repository_impl import UserRepositoryImpl
+from my_app.domain.repository_interfaces.user_repository import IUserRepository
+from my_app.domain.service.user_service import UserService
+from my_app.data.core import get_db
+
+DbSession = Annotated[Session, Depends(get_db)]
 
 
-from my_app.data.repositories.portfolio_repository_impl import PortfolioRepositoryImpl
-from my_app.domain.repository_interfaces.portfolio_repository import PortfolioRepository
+def get_user_repository(session: DbSession) -> IUserRepository:
+    return UserRepositoryImpl(session)
 
 
-def get_portfolio_repository() -> PortfolioRepository:
-    return PortfolioRepositoryImpl()
+def get_user_service(
+    user_repo: Annotated[IUserRepository, Depends(get_user_repository)],
+) -> UserService:
+    return UserService(user_repo)
 
 
-
+UserServiceDependency = Annotated[UserService, Depends(get_user_service)]
--- MySQL/my_app/api/router_config.py
+++ MySQL/my_app/api/router_config.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from my_app.api.routes import portfolio_route
+from my_app.api.routes import users
 
 api_router = APIRouter()
-api_router.include_router(portfolio_route.router)
+api_router.include_router(users.router)
