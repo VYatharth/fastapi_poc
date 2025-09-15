bdf799af94e1bd82175e58a15d9017becc4343f1
Yathartha <varshneya.yathartha@gmail.com>
2025-09-16 02:50:58 +0530

    added department route

--- MySQL/my_app/api/router_config.py
+++ MySQL/my_app/api/router_config.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from my_app.api.routes import users
+from my_app.api.routes import user_routes
+from my_app.api.routes import department_routes
 
 api_router = APIRouter()
-api_router.include_router(users.router)
+api_router.include_router(user_routes.router)
+api_router.include_router(department_routes.router)
